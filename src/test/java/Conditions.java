2b5e2066273830e30eab4363c23543ef5997f526
Autobot1k <57981768+Autobot1k@users.noreply.github.com>
2021-02-01 21:48:57 -0500

    02.01.2021
    1. Created test cases with loops using Array lists and conditions.
    2. Added  Cross-browser configurations.
    3. Modified TestNG.xml

--- src/test/java/Conditions.java
+++ src/test/java/Conditions.java
@@ -10,84 +10,133 @@ import java.util.List;
 public class Conditions extends BaseUI {
 
 //    @Test
-//    public void test(){
+//    public void test14() {
+//        String phrase = "Apple is inside of the list";
+//        List<String> num = new ArrayList<>(Arrays.asList("apple", "kiwi", phrase));
+//        num.add("lemon");
+//        System.out.println(num);
+//
+//        for (int i = 0; i < num.size(); i++) {
+//            String element = num.get(i);
+//
+//            System.out.println(i + " iterations");
+//
+//            if (element.contains("apple")) {
+//                System.out.println(phrase);
+//                break;
+//            }
+//            if (element.equals("orange")) {
+//                System.out.println("Orange");
+//            }
+//            if (element.contains("i")) {
+//                System.out.println("IIIII");
+//            } else {
+//                System.out.println("Bad loop");
+//            }
+//        }
+//    }
+//
+//    @Test
+//    public void test() {
 //        String fruit1 = "kiwi";
 //        String fruit2 = "kiwi";
 //
-//        if(fruit1.contains("kiwi") || fruit2.contains("orange")){
+//        if (fruit1.contains("kiwi") || fruit2.contains("orange")) {
 //            System.out.println("That's our fruits!");
-//        }else{
+//        } else {
 //            Assert.fail("No fruit!");
 //        }
 //    }
 //
 //    @Test
-//    public void test2(){
+//    public void test2() {
 //        int number = 10;
 //        int sum;
 //
-//        if(number == 10){
+//        if (number == 10) {
 //            sum = 95 + 5;
-//        }else{
+//        } else {
 //            sum = 95 - 5;
 //        }
 //        System.out.println(sum);
 //    }
 //
 //    @Test
-//    public void test3(){
+//    public void test3() {
 //        boolean requirement = false;
-//        if(!requirement){
+//        if (!requirement) {
 //            System.out.println("Boolean is true");
-//        }else{
+//        } else {
 //            Assert.fail("Boolean is not true");
 //        }
 //    }
 //
-    @Test
-    public void test4(){
-        WebElement tabSearch = driver.findElement(Locators.LINK_SEARCH);
-
-        if(tabSearch.isDisplayed()){
-            tabSearch.click();
-        }else{
-            Assert.fail("We can't find Pretty Women tab");
-        }
-    }
-
-    @Test
-    public void test5() {
-        mainPage.clickJoinButton();
-        mainPage.completeFirstPartOfRegistartion();
-
-        WebElement checkbox = driver.findElement(Locators.CHECKBOX_CONFIRMATION);
-
-        if(!checkbox.isSelected()){
-            checkbox.click();
-            System.out.println("Checkbox is selected");
-
-        }
-    }
-
 //    @Test
-//    public void test6() {
-////        List<String>list1 = new ArrayList<>(Arrays.asList("kiwi", "orange", "melon"));
-////        String element = list1.get(0);
+//    public void test4() {
+//        WebElement tabSearch = driver.findElement(Locators.LINK_SEARCH);
 //
-//        List<Integer>list1 = new ArrayList<>(Arrays.asList(18, 30));
-//        int sum = list1.get(0) + list1.get(1);
-//        System.out.println(sum);
+//        if (tabSearch.isDisplayed()) {
+//            tabSearch.click();
+//        } else {
+//            Assert.fail("We can't find Pretty Women tab");
+//        }
 //    }
+//
+//    @Test
+//    public void test5() {
+//        mainPage.clickJoinButton();
+//        mainPage.completeFirstPartOfRegistartion();
+//
+//        WebElement checkbox = driver.findElement(Locators.CHECKBOX_CONFIRMATION);
+//
+//        if (!checkbox.isSelected()) {
+//            checkbox.click();
+//            System.out.println("Checkbox is selected");
+//
+//        }
+//    }
+
+////    @Test
+////    public void test6() {
+//////        List<String>list1 = new ArrayList<>(Arrays.asList("kiwi", "orange", "melon"));
+//////        String element = list1.get(0);
+////
+////        List<Integer>list1 = new ArrayList<>(Arrays.asList(18, 30));
+////        int sum = list1.get(0) + list1.get(1);
+////        System.out.println(sum);
+////    }
+//
+
 
     @Test
     public void test7() {
+        String actualTitle;
+        String actualUrlPrettyWomen;
+        String expectedUrlPrettyWomen = "https://romanceabroad.com/users/search";
+        String expectedTitleHowWeWork = "Ukrainian women for marriage";
+        String expectedTitlePrettyWomen = "Single Ukrainian women online";
 
         List<WebElement> links = driver.findElements(Locators.MAIN_PAGE_LIST);
         System.out.println(links.size());
+
         for (int i = 0; i < links.size(); i++) {
+
             String info = links.get(i).getText();
             System.out.println(info);
             links.get(i).click();
+
+            if (info.contains("WORK")) {
+                actualTitle = driver.findElement(By.xpath("//h1")).getText();
+                Assert.assertEquals(actualTitle, expectedTitleHowWeWork);
+            }
+            if (info.contains("PRETTY WOMEN")) {
+                actualTitle = driver.findElement(By.xpath("//h1")).getText();
+                actualUrlPrettyWomen = driver.getCurrentUrl();
+                Assert.assertEquals(actualTitle, expectedTitlePrettyWomen);
+                Assert.assertEquals(actualUrlPrettyWomen, expectedUrlPrettyWomen);
+                driver.findElement(By.xpath("//a[@class='g-pic-border g-rounded']")).isDisplayed();
+            }
+
             driver.get(Data.MAIN_URL);
             links = driver.findElements(Locators.MAIN_PAGE_LIST);
 
@@ -95,69 +144,98 @@ public class Conditions extends BaseUI {
 
     }
 
-    @Test
-    public void test8() {
 
-        List<WebElement> list = driver.findElements(By.xpath("//ul//li"));
-        for (int i = 0; i < list.size(); i++) {
-            if (i == 3) {
-                list.get(i).click();
-            }
-        }
-    }
-
-
+    //
+//    @Test
+//    public void test8() {
+//
+//        List<WebElement> list = driver.findElements(By.xpath("//ul//li"));
+//        for (int i = 0; i < list.size(); i++) {
+//            if (i == 3) {
+//                list.get(i).click();
+//            }
+//        }
+//    }
+//
+//
     @Test
     public void test9() {
+        String actualUrlGifts = null;
+        String expectedUrlGifts = "https://romanceabroad.com/store/category-sweets";
+        WebElement giftsLink = driver.findElement(Locators.LINK_GIFTS);
+        List<WebElement> gifts = driver.findElements(By.xpath("//div[@class='title-block pt5']"));
+        List<String> options = new ArrayList<>(Arrays.asList("Spa", "Chocolate and fruits", "Teddy bear", "Flower basket", "Sushi sets", "TOURS TO UKRAINE"));
 
-        List<String> tabs = new ArrayList<>(Arrays.asList("GIFTS", "BLOG"));
-        for (int i = 0; i < tabs.size(); i++) {
-            System.out.println(tabs.get(i));
-            if (driver.findElement(Locators.LINK_GIFTS).getText().contains(tabs.get(i))) {
-                driver.findElement(Locators.LINK_GIFTS).click();
+        System.out.println(driver.findElements(Locators.MAIN_PAGE_LIST).size());
+        giftsLink.click();
+
+        System.out.println(driver.findElements(By.xpath("//div[@class='title-block pt5']")).size());
+
+        for (int i = 0; i < gifts.size(); i++) {
+
+            String info = gifts.get(i).getText();
+            System.out.println(info);
+            gifts.get(i).click();
+
+            if (info.contains(options.get(i)))
+            
+                actualUrlGifts = driver.getCurrentUrl();
+                Assert.assertEquals(actualUrlGifts, expectedUrlGifts);
+
             }
         }
     }
+//
 
-    @Test
-    public void test10() {
-        List<WebElement> list = driver.findElements(By.xpath("//ul//li"));
-        for (int i = 0; i <list.size(); i++) {
-            System.out.println(list.get(i).getText());
-            if(list.get(i).getText().contains("GIFTS")){
-                searchPage.clickPrettyWomenTab();
-                break;
-            }
-        }
-    }
-
-    @Test
-    public void test11(){
-        List<Integer> blackList = new ArrayList<>(Arrays.asList(3, 4, 5, 6, 7));
-        List<WebElement> tabs = driver.findElements(By.xpath("//ul[@class='navbar-nav']//li"));
-        for (int i = 0; i <tabs.size(); i++) {
-            System.out.println(tabs.get(i).getText());
-            if(!blackList.contains(i)) {
-                System.out.println("Print number of tab for click: " + i);
-                tabs.get(i).click();
-                driver.navigate().back();
-                tabs = driver.findElements(By.xpath("//ul[@class='navbar-nav']//li"));
-            }
-        }
-    }
-
-    @Test
-    public void test12() {
-        WebElement photo_tab = driver.findElement(Locators.LINK_PHOTOS);
-        photo_tab.click();
-        List<WebElement> tabs = driver.findElements(By.xpath("//ul[@class='b-tabs']//li"));
-        for (int i = 0; i < tabs.size(); i++) {
-            System.out.println(tabs.get(i).getText());
-            if(tabs.get(i).getText().contains("Photo")){
-                mediaPage.clickPhotoTab();
-                break;
-            }
-        }
-    }
-
-}
+//    @Test
+//    public void test10() {
+//        List<WebElement> list = driver.findElements(By.xpath("//ul//li"));
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i).getText());
+//            if (list.get(i).getText().contains("GIFTS")) {
+//                searchPage.clickPrettyWomenTab();
+//                break;
+//            }
+//        }
+//    }
+//
+//    @Test
+//    public void test11() {
+//        List<Integer> blackList = new ArrayList<>(Arrays.asList(3, 4, 5, 6, 7));
+//        List<WebElement> tabs = driver.findElements(By.xpath("//ul[@class='navbar-nav']//li"));
+//        for (int i = 0; i < tabs.size(); i++) {
+//            System.out.println(tabs.get(i).getText());
+//            if (!blackList.contains(i)) {
+//                System.out.println("Print number of tab for click: " + i);
+//                tabs.get(i).click();
+//                driver.navigate().back();
+//                tabs = driver.findElements(By.xpath("//ul[@class='navbar-nav']//li"));
+//            }
+//        }
+//    }
+//
+//    @Test
+//    public void test12() {
+//        WebElement photo_tab = driver.findElement(Locators.LINK_PHOTOS);
+//        photo_tab.click();
+//        List<WebElement> tabs = driver.findElements(By.xpath("//ul[@class='b-tabs']//li"));
+//        for (int i = 0; i < tabs.size(); i++) {
+//            System.out.println(tabs.get(i).getText());
+//            if (tabs.get(i).getText().contains("Photo")) {
+//                mediaPage.clickPhotoTab();
+//                break;
+//            }
+//        }
+//    }
+//
+//    @Test
+//    public void test13() {
+//        List<Integer> num = new ArrayList<>(Arrays.asList(1, 2, 3));
+//        for (int i = 0; i < num.size(); i++) {
+//            int element = num.get(i);
+//            System.out.println(element);
+//
+//        }
+//    }
+//
+//}
